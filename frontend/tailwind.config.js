c11f89e8badeccac4aa9683a3dda4d04b37701c2
redarmr <david.ren@nyu.edu>
2026-05-23 23:03:13 -0400

    Add generated app file: frontend/tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        ink: '#0f172a',
+        beacon: '#0ea5e9'
+      }
+    }
+  },
+  plugins: []
+};
